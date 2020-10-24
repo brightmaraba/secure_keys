3f1436ff04487d88b16d5c4b5c1abe202e1ee27a
Brian Koech <brightkoech@gmail.com>
2020-10-25 04:17:02 +0800

    Set access keys

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import tweepy
+import pandas as pd
+
+# Set API Keys required to access Twitter
+
+consumer_key = "sdfalkhjglshg234234325"
+consumer_secret = "3492375iuoahdoavoa"
+access_token = "sadfgagdsaghkhlaf"
+access_token_secret = "fdadfha323232"
+
+#! All above are fake keys for demonstration.
